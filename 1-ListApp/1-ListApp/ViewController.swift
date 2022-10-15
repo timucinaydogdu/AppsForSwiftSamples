e485c4c66e287d4520c0f5df477ce090f67f63b8
Timuçin AYDOĞDU <tim.aydogdu@gmail.com>
2022-10-15 17:03:09 +0300

    ListAppOnSwift Added

---
+++ 1-ListApp/1-ListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1-ListApp
+//
+//  Created by Timucin AYDOGDU on 15.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
